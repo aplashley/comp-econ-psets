46a71db1af0fe99691dc3d90e760f717a5cd6952
Tony Lashley <alashley15@gmail.com>
2015-05-06 15:24:09 -0500

    Prettied everything up significantly

---
+++ Comp Econ Problem Set 4/Scrap Work.R
@@ -0,0 +1,45 @@
+\section{Function Approximation}
+\subsection{Model for four-fifths of data, test on other fifth}
+
+<<>>==
+  
+  
+  
+  @
+
+\subsection{Model for all of data; overfit}
+
+<<>>==
+  
+  
+  
+  @
+
+
+\subsection{K-fold cross-validation}
+
+\section{Exploring data}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+stargazer(fit04, fit04m, fit04f, title = "Regressing Income on Yrs. of School and Exp.")
+stargazer(fit14, fit14m, fit14f)
+
